914a19deb35491ecba68779c2c0c2adfcc319438
codeme254 <otwomadennis365@gmail.com>
2022-11-21 10:33:19 +0300

    pass pycodestyle

---
+++ 1-pycodestyle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Just trying to be compliant with pycodestyle"""
+
+
+def check_code_style():
+    """Being compliant with Pycodestyle"""
+    print("Be compliant to pycodestyle and coding standards")
+
+
+check_code_style()
