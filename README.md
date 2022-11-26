4eda928f79e3a27b4505d0627a5e4c4b8be2a9c7
codeme254 <otwomadennis365@gmail.com>
2022-11-26 12:56:53 +0300

    fixing a fatal bug in progress

--- README.md
+++
@@ -1,4 +0,0 @@
-# AirBnB clone
-
-## A clone of the popular AirBnB site which is a marketplace focused on short-term homestays and experiences.
-
--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Helps to treat the models directory as a package
-"""
-
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
