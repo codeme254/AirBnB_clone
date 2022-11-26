3fe182934be69fd6db843ad247b04bc1db0e2a6b
codeme254 <otwomadennis365@gmail.com>
2022-11-26 12:22:25 +0300

    pycodestyle -1

--- 1-pycodestyle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Just trying to be compliant with pycodestyle"""
-
-
-def check_code_style():
-    """Being compliant with Pycodestyle"""
-    print("Be compliant to pycodestyle and coding standards")
