c76f93ee312b8f037093ba46b8da9fc87ce6a448
codeme254 <otwomadennis365@gmail.com>
2022-11-26 13:04:56 +0300

    pycodestyle checking final

---
+++ 1-pycodestyle-check.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Just writing a beautiful code that passes all the pycodetyle checks
+"""
+
+
+def check_pycodestyle():
+    """
+    Always observe coding standards
+    """
+    print("It's not about being fancy")
+    print("It's all about organization")
+
+
+check_pycodestyle()
